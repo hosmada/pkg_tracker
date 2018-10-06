4b855086305dac28f6069eb3fd43b46224c2297b
hosmada <usodamasijp@gmail.com>
2018-10-06 14:25:51 +0900

    add setup file

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import os
+
+from setuptools import setup, find_packages
+
+try:
+    with open('README.rst') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+# version
+here = os.path.dirname(os.path.abspath(__file__))
+version = next((line.split('=')[1].strip().replace("'", '')
+                for line in open(os.path.join(here,
+                                              'circleci-requirements-pr',
+                                              '__init__.py'))
+                if line.startswith('__version__ = ')),
+               '0.0.dev0')
+
+setup(
+    name="pypipkg",
+    version=version,
+    url='https://github.com/user/pypipkg',
+    author='hosmada',
+    author_email='usodamasijp@gmail.com',
+    maintainer='hosmada',
+    maintainer_email='usodamasijp@gmail.com',
+    description='open pull request via circle-ci',
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=_requires_from_file('requirements.txt'),
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
